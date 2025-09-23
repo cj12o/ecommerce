1b2c2e1438a44a4e2ec2d03a4f66efb300cd03fe
cj12o <cj2004jee@gmail.com>
2025-09-23 23:53:04 +0530

    commit 3: cleaned code

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -8,26 +8,22 @@ const loginUser=async(req,resp)=>{
         const {email,password}=req.body
     
         if(!email || !password) throw new Error("Error in Authentication,Incomplete Details")
-        // const emailReg=await db.client.query(`Select email from user Where email=${email}`)
+       
         const emailRegObj=await db.conn.select({email:userSchema.email}).from(userSchema).where(eq(userSchema.email,email));
-        // console.log(`EMAIL REG=${emailReg.map((obj)=>{
-        //     console.log(obj)
-        // })}`)
-        // const emailReg=Object.values(emailRegObj)
         const emailReg=emailRegObj[0]['email']
-        console.log("Email registered==>>",emailReg)
+        
         if(!emailReg) throw new Error("No such User")
-        // const passwordReg=await db.client.query(`Select password from user Where email=${email}`)
+        
         const passwordRegObj=await db.conn.select({password:userSchema.password}).from(userSchema).where(eq(userSchema.email,email))
         const passwordReg=passwordRegObj[0]['password']
-        console.log("password",passwordReg)
+
         if(passwordReg!=password) throw new Error("Invalid credentials")
         return resp
             .status(200)
             .json(new Apiresponse(200,{
                 user:email
             }
-            ,"user Succesfully loged in"))
+            ,"user Succesfully logged in"))
     }
     catch(e){
         const error_lst=[]
